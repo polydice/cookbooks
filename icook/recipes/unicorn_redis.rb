4c2ea05b6e0456ca9d5f93b2c52f15c685a74e15
Richard Lee <dlackty@gmail.com>
2013-03-29 05:17:34 +0800

    Fix mismatch of unicorn config.

--- icook/recipes/unicorn_redis.rb
+++ icook/recipes/unicorn_redis.rb
@@ -11,7 +11,7 @@ node[:deploy].each do |application, deploy|
   ruby_block "insert_reconnecting_line" do
     block do
       file = Chef::Util::FileEdit.new("#{deploy[:deploy_to]}/shared/config/unicorn.conf")
-      file.insert_line_after_match("# correctly implements pread()/pwrite() system calls)", "$redis.client.reconnect")
+      file.insert_line_after_match(Regexp.quote("# correctly implements pread()/pwrite() system calls)"), "  $redis.client.reconnect")
       file.write_file
     end
   end
