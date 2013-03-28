9fbb1f151f8db0b40bb5e6163014c5721fd9398b
Richard Lee <dlackty@gmail.com>
2013-03-29 03:45:20 +0800

    Add unicorn reconnection setting recipe.

---
+++ icook/recipes/unicorn_redis.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: icook
+# Recipe:: g
+#
+# Copyright 2012, Richard Lee
+#
+# All rights reserved - Do Not Redistribute
+#
+
+node[:deploy].each do |application, deploy|
+  ruby_block "insert_reconnecting_line" do
+    block do
+      file = Chef::Util::FileEdit.new("#{deploy[:deploy_to]}/shared/config/unicorn.conf")
+      file.insert_line_after_match("# correctly implements pread()/pwrite() system calls)", "$redis.client.reconnect")
+      file.write_file
+    end
+  end
+end
