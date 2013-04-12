5227b0bf383c677c41e86596974a2aa8cb1385e0
Richard Lee <dlackty@gmail.com>
2013-04-12 18:01:19 +0800

    Fix deploy capistrano recipe.

--- icook/recipes/capistrano.rb
+++ icook/recipes/capistrano.rb
@@ -8,6 +8,16 @@
 #
 
 node[:deploy].each do |application, deploy|
+  opsworks_deploy_user do
+    deploy_data deploy
+  end
+
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+
   %w{ assets bundle public tmp }.each do |dir|
     directory "#{deploy[:deploy_to]}/shared/#{dir}" do
       owner deploy[:user]
