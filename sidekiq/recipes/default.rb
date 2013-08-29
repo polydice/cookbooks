bacbf0e76df55a95d3bda042425ac5c8d6b23611
Richard Lee <dlackty@gmail.com>
2013-08-29 23:58:07 +0800

    Remove sidekiq-web from cookbook.

--- sidekiq/recipes/default.rb
+++ sidekiq/recipes/default.rb
@@ -15,24 +15,13 @@ node[:deploy].each do |application, deploy|
     })
   end
 
-  template "#{node[:monit][:conf_dir]}/sidekiq_web_#{application}.monitrc" do
-    owner 'root'
-    group 'root'
-    mode 0644
-    source "web_monitrc.conf.erb"
-    variables({
-      :app_name => application,
-      :deploy => deploy
-    })
-  end
-
   execute "ensure-sidekiq-is-setup-with-monit" do
     command %Q{
       monit reload
     }
   end
 
-    execute "restart-sidekiq" do
+  execute "restart-sidekiq" do
     command %Q{
       echo "sleep 20 && monit -g sidekiq_#{application} restart all" | at now
     }
