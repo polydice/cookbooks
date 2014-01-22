823063ecad25e8881646db3464f0c2036076bcc2
Bert Chang <jrweizhang@gmail.com>
2014-01-22 15:20:05 +0800

    Add s3.json template.

--- deploy/recipes/knife.rb
+++ deploy/recipes/knife.rb
@@ -17,6 +17,20 @@ node[:deploy].each do |application, deploy|
     app application
   end
 
+  if node[:s3_config]
+    template "#{deploy[:current_path]}/config/s3.json" do
+      source "s3.json.erb"
+      mode 0755
+      group deploy[:group]
+      owner deploy[:user]
+
+      variables(
+        "access_key" => node[:s3_config][:access_key],
+        "secret_key" => node[:s3_config][:secret_key]
+      )
+    end
+  end
+
   execute "build from the go source of #{application}" do
     command "export GOPATH=/home/deploy/source"
     cwd deploy[:current_path]
