e0ef425380b8150dcec961bfc631915098137d8b
Bert Chang <jrweizhang@gmail.com>
2014-01-27 23:46:10 +0800

    Fix notify recipe.

--- knife/recipes/notify_peer.rb
+++ knife/recipes/notify_peer.rb
@@ -1,14 +1,12 @@
 peers = []
-node[:opsworks][:layers][:go_app_server].each do |_, instance|
+node[:opsworks][:layers][:go_app_server][:instances].each do |key, instance|
   peers << "http://#{instance[:private_ip]}:50000"
 end
 
 bash "update-gc-peers" do
-  cwd release_path
   code <<-EOH
-  echo "updating gc peers"
-  curl -X POST -d #{peers.join("&")} http://127.0.0.1:3000/peers
-  cp #{node[:deploy][:deploy_to]}/shared/config/* #{node[:deploy][:deploy_to]}/current/config/
+  curl -X POST -d "#{peers.join("&")}" http://127.0.0.1:3000/peers
+  cp #{node[:deploy][:deploy_to]}/shared/config/s3.json #{node[:deploy][:deploy_to]}/current/config/s3.json
   EOH
   action :run
 end
