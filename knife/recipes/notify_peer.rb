1156d158eefe7bc98ad9df0f8e6c04e41a904ee0
Bert Chang <jrweizhang@gmail.com>
2014-02-06 13:22:32 +0800

    Fix typo and apply dynamic config.

--- knife/recipes/notify_peer.rb
+++ knife/recipes/notify_peer.rb
@@ -1,11 +1,11 @@
 peers = []
 node[:opsworks][:layers][:go_app_server][:instances].each do |key, instance|
-  peers << "peers=http://#{instance[:pirvate_ip]}:5000"
+  peers << "peers=http://#{instance[:private_ip]}:#{node[:application_config][:gc_httppool_port]}"
 end
 
 bash "update-gc-peers" do
   code <<-EOH
-  curl -X POST -d "#{peers.join("&")}" http://127.0.0.1:3000/peers
+  curl -X POST -d "#{peers.join("&")}" http://127.0.0.1:#{node[:application_config][:peer_manager_port]}/peers
   cp #{node[:deploy][:knife][:deploy_to]}/shared/config/* #{node[:deploy][:knife][:deploy_to]}/current/config/
   EOH
   action :run
