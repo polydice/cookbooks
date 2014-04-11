1406a3da4cf664cf755cce4048f4bf9b275b9988
Richard Lee <dlackty@gmail.com>
2014-04-11 15:03:34 +0800

    Move cookbooks to Berkshelf

--- sysctl/attributes/default.rb
+++
@@ -1,8 +0,0 @@
-case platform_family
-when 'debian', 'rhel'
-  default['sysctl']['conf_dir'] = '/etc/sysctl.d'
-else
-  default['sysctl']['conf_dir'] = nil
-end
-default['sysctl']['params'] = {}
-default['sysctl']['allow_sysctl_conf'] = false
--- ulimit/attributes/default.rb
+++
@@ -1,2 +0,0 @@
-default['ulimit']['pam_su_template_cookbook'] = nil
-default['ulimit']['users'] = Mash.new
