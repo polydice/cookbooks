fd8bafddb7bfc025b08775f4c4118edaa0445b4d
Richard Lee <dlackty@gmail.com>
2014-07-24 15:55:01 +0800

    Use shell for dotenv

--- dotenv/libraries/dotenv.rb
+++ dotenv/libraries/dotenv.rb
@@ -8,7 +8,7 @@ module Dotenv
     if node.is_a? Hash
       node.map { |k, v| walk(v, parents + [k]) }.flatten
     else # a Hash
-      "#{(parents).map(&:upcase).join('_')}=#{node}"
+      "export #{(parents).map(&:upcase).join('_')}=#{node}"
     end
   end
 end
--- dotenv/recipes/default.rb
+++ dotenv/recipes/default.rb
@@ -5,10 +5,10 @@
 
 node[:deploy].each do |application, deploy|
   if deploy[:env]
-    file "/srv/www/#{application}/shared/.env" do
-      mode 0644
-      owner deploy[:user]
-      group deploy[:group]
+    file "/etc/profile.d/#{application}.sh" do
+      mode 0755
+      owner "root"
+      group "root"
       action :create
       content Dotenv.walk(deploy[:env]).join("\n")
     end
