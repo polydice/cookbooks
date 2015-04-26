02c6f811fd3560c6db8868f4028b168df664ee68
frozenfung <frozenfung@gmail.com>
2015-04-26 13:06:43 +0800

    Set ik config

--- elasticsearch_config/recipes/default.rb
+++ elasticsearch_config/recipes/default.rb
@@ -13,6 +13,18 @@ include_recipe "elasticsearch"
 include_recipe "elasticsearch::monit"
 include_recipe "elasticsearch::plugins"
 
+if url = node[:elasticsearch][:corpus][:ik]
+  script "install_ik_data" do
+    interpreter "bash"
+    user "root"
+    cwd "/usr/local/etc/elasticsearch"
+    code <<-EOH
+      wget http://devops.polydice.com.s3.amazonaws.com/ik.tar.gz
+      tar -zxf ik.tar.gz
+    EOH
+  end
+end
+
 
 
 
