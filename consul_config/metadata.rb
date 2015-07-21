e092e3548bfd3d14c30b2d8cdd3fb5ad8da981f1
Richard Lee <dlackty@gmail.com>
2015-07-21 15:58:34 +0800

    Add consul_config for wordpress

---
+++ consul_config/metadata.rb
@@ -0,0 +1,8 @@
+name             "consul_config"
+maintainer       "Richard Lee"
+maintainer_email "richard@polydice.com"
+license          "Apache 2.0"
+description      "Configures consul."
+version          "0.1.0"
+
+depends "consul"
