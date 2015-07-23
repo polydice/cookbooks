96c0163db599a83c13493f2ac8d8395bd700904f
frozenfung <frozenfung@gmail.com>
2015-07-23 12:58:54 +0800

    Add attribute for script position

--- consul_config/recipes/sidekiq.rb
+++ consul_config/recipes/sidekiq.rb
@@ -1,11 +1,9 @@
-if url = node[:consul][:sidekiq_script_url]
-  remote_file "/usr/local/bin/check_sidekiq" do
-    source url
-    owner "root"
-    group "root"
-    mode "0755"
-    action :create
-  end
+remote_file "/usr/local/bin/check_sidekiq" do
+  source node[:consul][:config]
+  owner "root"
+  group "root"
+  mode "0755"
+  action :create
 end
 
 consul_check_def "sidekiq" do
