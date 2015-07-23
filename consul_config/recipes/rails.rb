e51218d76d6946f06a5803125d8e48debcf8bb1e
Stan Low <a22301613@yahoo.com.tw>
2015-07-23 17:55:23 +0800

    Improve rails recipe

--- consul_config/recipes/rails.rb
+++ consul_config/recipes/rails.rb
@@ -1,5 +1,4 @@
-consul_service_def node[:consul][:app_name] do
-  port 80
+consul_service_def node[:opsworks][:applications][0][:slug_name] do
   tags ["rails"]
   check(
     http "http://localhost:80/rack_health",
