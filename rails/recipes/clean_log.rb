1bf975697b343cc4f8d8d1942755fe02eaf54102
frozenfung <frozenfung@gmail.com>
2015-08-27 11:55:55 +0800

    Improve syntax

--- rails/recipes/clean_log.rb
+++ rails/recipes/clean_log.rb
@@ -1,18 +1,21 @@
-file "/var/log/td-agent/td-agent.log.1" do
-  action :delete
-  backup false
-  only_if { File.exists? "/var/log/td-agent.log.1" }
-end
-
 file "/var/log/nginx/icook.tw.access.log.1" do
   action :delete
   backup false
   only_if { File.exists? "/var/log/nginx/icook.tw.access.log.1" }
 end
 
-%w[ /var/log/td-agent/td-agent.log.*.gz /var/log/nginx/icook.tw.access.log.*.gz ].each do |path|
-  file path do
-    action :delete
-    backup false
+file "/var/log/td-agent/td-agent.log.1" do
+  action :delete
+  backup false
+  only_if { File.exists? "/var/log/td-agent/td-agent.log.1" }
+end
+
+bash "Remove Log Files" do
+  user "root"
+  %w[ /var/log/td-agent/td-agent.log.*.gz /var/log/nginx/icook.tw.access.log.*.gz ].each do |files|
+    code <<-EOH
+      rm #{files}
+    EOH
+    Chef::Log.info("#{files} are eliminated.")
   end
 end
