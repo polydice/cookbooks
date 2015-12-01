d6a5cc7087586631a8fd69266fe0f8ce9362c0df
Richard Lee <dlackty@gmail.com>
2015-12-02 01:28:09 +0800

    Fix resource order issue

--- librato-collectd/recipes/default.rb
+++ librato-collectd/recipes/default.rb
@@ -1,26 +1,23 @@
-#
-# Cookbook Name:: librato-collectd
-# Recipe:: default
-#
-# Copyright 2015, Richard Lee
-#
-# All rights reserved - Do Not Redistribute
-#
-
 packagecloud_repo "librato/librato-collectd" do
   type "deb"
 end
 
-package "collectd"
+ruby_block 'librato-credentials' do
+  block do
+    f = Chef::Util::FileEdit.new('/opt/collectd/etc/collectd.conf.d/librato.conf')
+    f.search_file_replace(%r{User ""},
+                          "User \"#{node[:librato][:collectd][:user]}\"")
+    f.search_file_replace(%r{Password ""},
+                          "Password \"#{node[:librato][:collectd][:password]}\"")
+    f.write_file
+  end
 
-file '/opt/collectd/etc/collectd.conf.d/librato.conf' do
-  f = Chef::Util::FileEdit.new(path)
-  f.search_file_replace(%r{User ""},
-                        "User \"#{node[:librato][:collectd][:user]}\"")
-  f.search_file_replace(%r{Password ""},
-                        "Password \"#{node[:librato][:collectd][:password]}\"")
-  f.write_file
   notifies :restart, "service[collectd]"
+  action :nothing
+end
+
+package "collectd" do
+  notifies :run, "ruby_block[librato-credentials]"
 end
 
 include_recipe 'librato-collectd::_service'
